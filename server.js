113834664265a36c236254f85ca332494feb8b2d
Krittiya <krittiyaclark@gmail.com>
2021-12-18 11:25:05 -0500

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({ path: './config/.env' })
 
 connectDB()
 
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use('/', homeRoutes)
 app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Server is running, you better catch it!')
+})
